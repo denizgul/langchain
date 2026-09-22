c2d5f1d49f42f546d46be4d8b34240ca80f6236a
denizgul <denizgul@gmail.com>
2026-09-22 18:37:15 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,22 +1,62 @@
 # This is a sample Python script.
+from typing import cast
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 #
 #
-from dotenv import load_dotenv, dotenv_values
+
+
+from dotenv import load_dotenv
+from langchain.chains.summarize.map_reduce_prompt import prompt_template
 
 load_dotenv()
 
-#print(dotenv_values(".env").get("OPENAI_API_KEY"))
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from fastapi import FastAPI
+from langserve import add_routes
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+#temperature randomness ve creativity
+model = ChatOpenAI(model="gpt-4",temperature=0.1);
+#messages = [
+ #   SystemMessage(content="Translate the following from English to Spanish"),
+#  HumanMessage(content="Hi"),
+#]
+
+system_prompt = "Translate the following into {language}"
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        ("system",system_prompt),("user","{text}")
+    ]
+)
+
+parser = StrOutputParser()
+#response = model.invoke(messages)
+
+chain = prompt_template | model | parser
+
+app =  FastAPI(
+    title="Translate into {language}",
+    version="0.1.0",
+    description="Translate into {language}",
+)
+
+add_routes(
+    app,
+    chain,
+    path="/chain"
+)
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+ #   response = model.invoke(messages)
+ #   print(chain.invoke({"language":"Italian", "text":"Hi"}))
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=8000)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
